799158ea006bfdabfecec51bc1c93cfec85e9cd3
Kerolos Sadek <kokosadek55@gmail.com>
2022-09-21 13:43:36 +0200

    Session-13-Angular

--- src/app/components/app.module.ts
+++ src/app/components/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SliderComponent } from './slider/slider.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    BlogsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/blogs/blogs.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blogs',
+  templateUrl: './blogs.component.html',
+  styleUrls: ['./blogs.component.css']
+})
+export class BlogsComponent implements OnInit {
+  firstName:string = "Kerolos"
+  inputType:string = "password"
+  imagePath: string = "assets/images/download.jpg"
+  inputVal: string = ""
+  val : number = 0
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  getNumber(){
+    return 200
+  }
+
+  changeName(){
+    this.firstName = "Sadek"
+  }
+  handleInput(event:any){
+  }
+  add(){
+    this.val +=  1
+  }
+  sub(){
+    this.val -= 1
+  }
+}
+
